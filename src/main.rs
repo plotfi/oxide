49d13f347ba1777424cf3db1b1a5210250a77e44
Puyan Lotfi <plotfi@fb.com>
2022-02-16 20:38:08 -0800

    get things to compile again

--- src/main.rs
+++ src/main.rs
@@ -33,20 +33,21 @@ fn get_mime_type(filename: String) -> String {
 fn send_headers(status: i32, title: String, mime: String,
                 socket: i32, len: u32) -> std::string::String {
 
-  let mut HeaderStr =
-    format!("HTTP/1.1 {} {} \r\nServer: swift-httpd\r\n", status, title) +
-  match mime.as_str() {
-    "" => { format!("Content-Type: {} \r\n", mime) },
-    _  => { "" }
-  };
+  let mut header =
+    format!("HTTP/1.1 {} {} \r\nServer: swift-httpd\r\n", status, title);
 
-  if len >= 0 {
-    HeaderStr += format!("Content-Length: {}\r\n", len);
+  let mime_header = format!("Content-Type: {} \r\n", mime);
+  let length_header = format!("Content-Length: {}\r\n", len);
+
+  if mime != "" {
+    header += mime_header.as_str();
   }
 
-  HeaderStr += "Connection: close\r\n\r\n";
+  if len != 0 {
+    header += length_header.as_str();
+  }
 
-  HeaderStr
+  header + "Connection: close\r\n\r\n"
 }
 
 fn main() {
